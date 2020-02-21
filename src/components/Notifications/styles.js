27c98fee6a6c24fcf1d1107096905c2267a0113b
tukno <oliveiradavid007@gmail.com>
2020-02-21 19:23:05 -0400

    chore: Set higher contrast notifications list

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -32,10 +32,11 @@ export const NotificationList = styled.div`
   width: 260px;
   left: calc(50% - 130px);
   top: calc(100% + 30px);
-  background: rgba(0, 0, 0, 0.6);
+  background: rgba(0, 0, 0, 0.9);
   border-radius: 4px;
   padding: 15px 5px;
   display: ${props => (props.visible ? 'block' : 'none')};
+  z-index: 999;
 
   /* triangulo */
   &::before {
