833cd4cfa774abdcf42486235fc6b7db48d51791
tukno <oliveiradavid007@gmail.com>
2020-02-29 05:50:43 -0400

    chore: Include files directory in version control

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333'
+      : 'https://tukno-gobarber-api.herokuapp.com',
 });
 
 export default api;
